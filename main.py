269c27a2e68a290753673d209b658128b3c0a413
jpalombo1 <jpyanks95@gmail.com>
2023-01-30 01:20:36 -0500

    cleanup

--- main.py
+++ main.py
@@ -6,16 +6,19 @@ Created on Sun Aug 12 23:59:25 2018
 @author: josephpalombo
 """
 from poker.card import Card
-from poker.common import CardsLeft
+from poker.common import CardsLeft, Suits, Values
 from poker.deck import generate_deck, generate_hand, remove_from_deck
 from poker.evaluate import prob_win
 
-TESTS = 100
+TESTS = 10
 NUM_PLAYERS = 5
 
 
 def main():
-    """."""
+    """Main function to play start, flop, river, turn for given number of hands.
+
+    Make deck, generate hands from it for each player, play turn and calculate win probability until end of game.
+    """
     for _ in range(TESTS):
         deck = generate_deck()
         player_hands: list[list[Card]] = []
@@ -62,115 +65,115 @@ def main():
 
 
 def test():
-    from poker.common import Suits, Values
-
-    deck = generate_deck()
-    player_hands = [
+    """Special test cases to verify working functionality."""
+    CASES = [
         [
-            Card(Suits.DIAMOND, Values.NINE),
-            Card(Suits.DIAMOND, Values.FOUR),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.KING),
-            Card(Suits.CLUB, Values.FIVE),
-            Card(Suits.HEART, Values.ACE),
+            [
+                Card(Suits.DIAMOND, Values.NINE),
+                Card(Suits.DIAMOND, Values.FOUR),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.KING),
+                Card(Suits.CLUB, Values.FIVE),
+                Card(Suits.HEART, Values.ACE),
+            ],
+            [
+                Card(Suits.DIAMOND, Values.EIGHT),
+                Card(Suits.CLUB, Values.SIX),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.KING),
+                Card(Suits.CLUB, Values.FIVE),
+                Card(Suits.HEART, Values.ACE),
+            ],
+            [
+                Card(Suits.DIAMOND, Values.JACK),
+                Card(Suits.DIAMOND, Values.KING),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.KING),
+                Card(Suits.CLUB, Values.FIVE),
+                Card(Suits.HEART, Values.ACE),
+            ],
+            [
+                Card(Suits.HEART, Values.SEVEN),
+                Card(Suits.SPADE, Values.KING),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.KING),
+                Card(Suits.CLUB, Values.FIVE),
+                Card(Suits.HEART, Values.ACE),
+            ],
+            [
+                Card(Suits.CLUB, Values.TWO),
+                Card(Suits.SPADE, Values.FOUR),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.KING),
+                Card(Suits.CLUB, Values.FIVE),
+                Card(Suits.HEART, Values.ACE),
+            ],
         ],
         [
-            Card(Suits.DIAMOND, Values.EIGHT),
-            Card(Suits.CLUB, Values.SIX),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.KING),
-            Card(Suits.CLUB, Values.FIVE),
-            Card(Suits.HEART, Values.ACE),
-        ],
-        [
-            Card(Suits.DIAMOND, Values.JACK),
-            Card(Suits.DIAMOND, Values.KING),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.KING),
-            Card(Suits.CLUB, Values.FIVE),
-            Card(Suits.HEART, Values.ACE),
-        ],
-        [
-            Card(Suits.HEART, Values.SEVEN),
-            Card(Suits.SPADE, Values.KING),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.KING),
-            Card(Suits.CLUB, Values.FIVE),
-            Card(Suits.HEART, Values.ACE),
-        ],
-        [
-            Card(Suits.CLUB, Values.TWO),
-            Card(Suits.SPADE, Values.FOUR),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.KING),
-            Card(Suits.CLUB, Values.FIVE),
-            Card(Suits.HEART, Values.ACE),
+            [
+                Card(Suits.HEART, Values.SEVEN),
+                Card(Suits.DIAMOND, Values.THREE),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.EIGHT),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.ACE),
+                Card(Suits.CLUB, Values.TEN),
+            ],
+            [
+                Card(Suits.DIAMOND, Values.KING),
+                Card(Suits.HEART, Values.THREE),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.EIGHT),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.ACE),
+                Card(Suits.CLUB, Values.TEN),
+            ],
+            [
+                Card(Suits.HEART, Values.SIX),
+                Card(Suits.CLUB, Values.QUEEN),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.EIGHT),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.ACE),
+                Card(Suits.CLUB, Values.TEN),
+            ],
+            [
+                Card(Suits.DIAMOND, Values.SEVEN),
+                Card(Suits.CLUB, Values.KING),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.EIGHT),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.ACE),
+                Card(Suits.CLUB, Values.TEN),
+            ],
+            [
+                Card(Suits.SPADE, Values.SIX),
+                Card(Suits.CLUB, Values.ACE),
+                Card(Suits.SPADE, Values.NINE),
+                Card(Suits.HEART, Values.EIGHT),
+                Card(Suits.DIAMOND, Values.SIX),
+                Card(Suits.SPADE, Values.ACE),
+                Card(Suits.CLUB, Values.TEN),
+            ],
         ],
     ]
 
-    player_hands = [
-        [
-            Card(Suits.HEART, Values.SEVEN),
-            Card(Suits.DIAMOND, Values.THREE),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.EIGHT),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.ACE),
-            Card(Suits.CLUB, Values.TEN),
-        ],
-        [
-            Card(Suits.DIAMOND, Values.KING),
-            Card(Suits.HEART, Values.THREE),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.EIGHT),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.ACE),
-            Card(Suits.CLUB, Values.TEN),
-        ],
-        [
-            Card(Suits.HEART, Values.SIX),
-            Card(Suits.CLUB, Values.QUEEN),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.EIGHT),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.ACE),
-            Card(Suits.CLUB, Values.TEN),
-        ],
-        [
-            Card(Suits.DIAMOND, Values.SEVEN),
-            Card(Suits.CLUB, Values.KING),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.EIGHT),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.ACE),
-            Card(Suits.CLUB, Values.TEN),
-        ],
-        [
-            Card(Suits.SPADE, Values.SIX),
-            Card(Suits.CLUB, Values.ACE),
-            Card(Suits.SPADE, Values.NINE),
-            Card(Suits.HEART, Values.EIGHT),
-            Card(Suits.DIAMOND, Values.SIX),
-            Card(Suits.SPADE, Values.ACE),
-            Card(Suits.CLUB, Values.TEN),
-        ],
-    ]
-
-    for player_hand in player_hands:
-        deck = remove_from_deck(deck, player_hand)
-    print(len(deck))
-    win_probs = [
-        prob_win(player_hands, deck, CardsLeft.RIVER, pnum)
-        for pnum in range(NUM_PLAYERS)
-    ]
-    print(win_probs)
+    for player_hands in CASES:
+        deck = generate_deck()
+        for player_hand in player_hands:
+            deck = remove_from_deck(deck, player_hand)
+        win_probs = [
+            prob_win(player_hands, deck, CardsLeft.RIVER, pnum)
+            for pnum in range(len(player_hands))
+        ]
+        print(win_probs)
 
 
 if __name__ == "__main__":
     main()
-    # test()
+    test()
